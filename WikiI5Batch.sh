a39ed2d5fc65ad2bf09f41b26131a9674c056f70
margaretha <margaretha@ids-mannheim.de>
2016-07-20 14:17:45 +0200

    Added more folder checking.

--- WikiI5Batch.sh
+++ WikiI5Batch.sh
@@ -58,21 +58,28 @@ date=$3
 filename=$lang$wiki-$date-$type
 properties=code/properties/$type/xml-$lang$wiki-$type.properties
 
-echo "Converting wikitext to WikiXML" $filename
-#echo $properties
-#nice -n 3 java -jar -Xmx4g $wikixml -prop $properties > logs/wikiXML-$filename.log 2>&1
+if [ ! -d "logs" ];
+then mkdir logs;
+fi
 
-#./code/WikiXMLCorpusIndexer.sh $2 wikixml-$lang/$2/ wikixml-$lang/$2-index.xml
-
-echo "Converting wikiXML to I5"
-main=de.mannheim.ids.wiki.WikiI5Converter
-prop=code/properties/$type/i5-$lang$wiki-$type.properties
-#nice -n 3 java -Xmx4g -cp "code/WikiI5Converter-1.0.1-jar-with-dependencies.jar:lib/*:." $main -prop $prop > logs/wikiI5-$filename.log 2>&1 
+if [ ! -d "i5" ];
+then mkdir i5;
+fi
 
 if [ ! -d "i5/$1" ];
 then
     mkdir i5/$1
 fi
+
+echo "Converting wikitext to WikiXML" $filename
+#echo $properties
+nice -n 3 java -jar -Xmx4g $wikixml -prop $properties > logs/wikiXML-$filename.log 2>&1
+./code/WikiXMLCorpusIndexer.sh $2 wikixml-$lang/$2/ wikixml-$lang/$2-index.xml
+
+echo "Converting wikiXML to I5"
+main=de.mannheim.ids.wiki.WikiI5Converter
+prop=code/properties/$type/i5-$lang$wiki-$type.properties
+nice -n 3 java -Xmx4g -cp "code/WikiI5Converter-1.0.1-jar-with-dependencies.jar:lib/*:." $main -prop $prop > logs/wikiI5-$filename.log 2>&1 
 
 echo "Replacing invalid Chars"
  #sed -i -e 's/&#xd[8-9a-f][0-9a-f][0-9a-f];/ /g' i5/$filename.i5.xml; 
@@ -80,6 +87,7 @@ perl -wlnpe 's/\&#xd[89a-f]..;/\&#xf8ff;/g' < i5/$filename.i5.xml > i5/$lang/$fi
 
 echo "Validating against xmllint"
 xmllint -valid -stream i5/$lang/$filename.i5.xml > logs/xmllint-$filename.i5.log 2>&1; 
+
 echo "Validating against onsgmls"
 onsgmls -E0 -wxml -s -c /usr/share/sgml/xml.soc i5/$lang/$filename.i5.xml > logs/onsgmls-$filename.i5.log 2>&1
 
