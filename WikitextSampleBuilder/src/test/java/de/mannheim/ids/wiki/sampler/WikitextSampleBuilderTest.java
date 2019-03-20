9986abde8c693a526505e53b0da54d3e2bf959d1
margaretha <margaretha@ids-mannheim.de>
2019-03-20 17:31:46 +0100

    Updated Wikitext sample builder test.

--- WikitextSampleBuilder/src/test/java/de/mannheim/ids/wiki/sampler/WikitextSampleBuilderTest.java
+++ WikitextSampleBuilder/src/test/java/de/mannheim/ids/wiki/sampler/WikitextSampleBuilderTest.java
@@ -1,31 +1,41 @@
 package de.ids_mannheim.korap.sampler;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.cli.ParseException;
+import org.junit.Test;
 
+import de.mannheim.ids.wiki.sampler.WikitextSampler;
 import junit.framework.TestCase;
 
-/**
- * Unit test for WikitextSampler
- */
-public class WikitextSamplerTest extends TestCase {
+public class WikitextSampleBuilderTest {
 
-	private String wikidump = "data/dewiki-20170701-pages.xml";
+	private String wikidump = "../WikiXMLConverter/data/dewiki-20170701-sample.xml";
 	private String output = "dewiki-20170701-sample.xml";
 
+	@Test
 	public void testSamplingWithDefaultFactor()
 			throws ParseException, IOException {
 		WikitextSampler sampler = new WikitextSampler();
 		sampler.parseArguments(new String[]{"-i", wikidump, "-o", output});
 		sampler.createSample();
+		
+		File f = new File(output);
+		assertTrue(f.exists());
 	}
 
+	@Test
 	public void testSamplingWithSpecificFactorAndWeight()
 			throws ParseException, IOException {
 		WikitextSampler sampler = new WikitextSampler();
 		sampler.parseArguments(new String[]{"-i", wikidump, "-f",
-				"0.00001", "-rw", "0.05", "-lw", "0.001"});
+				"0.1", "-rw", "0.5", "-lw", "0.5"});
 		sampler.createSample();
+		
+		File f = new File("output.xml");
+		assertTrue(f.exists());
 	}
 }
