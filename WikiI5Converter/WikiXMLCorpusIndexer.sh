52443d12c627900610fe8be75b9106668b0f1198
margaretha <margaretha@ids-mannheim.de>
2021-11-15 15:23:59 +0100

    Added output folder existance check.

--- WikiI5Converter/WikiXMLCorpusIndexer.sh
+++ WikiI5Converter/WikiXMLCorpusIndexer.sh
@@ -1,13 +1,13 @@
 # WikiXML Corpus Indexer
 # -------------------------------------------------------------------
-# This script lists all the WikiXML files, that are outputs of the 
-# WikiXMLConverter tool. The WikiXMLConverter generates WikiXML files 
-# under xml-[language] folder. Moreover, the WikiXML pages are grouped 
+# This script lists all the WikiXML files, that are outputs of the
+# WikiXMLConverter tool. The WikiXMLConverter generates WikiXML files
+# under xml-[language] folder. Moreover, the WikiXML pages are grouped
 # numerically and alphabetically.
-# 
+#
 # Input parameters:
 # - pageType [e.g. article, talk, user-talk]
-# - path to xmlFolder, for example: wikixml-de/article to index the 
+# - path to xmlFolder, for example: wikixml-de/article to index the
 #   German WikiXML article pages
 # - output file
 #
@@ -30,7 +30,7 @@
 #    [....]
 # </wiki>
 #
-# The index is used in WikiI5Converter for structuring the Wikipedia 
+# The index is used in WikiI5Converter for structuring the Wikipedia
 # pages in the resulting Wikipedia I5 corpus file.
 #
 # Eliza Margaretha
@@ -44,11 +44,11 @@
 
 function createList {
     echo "<wiki type=\""$1"\">" > $3
-    
+
     for index in $(find -L $2 -type d | sed 's/.*\///' | sort);
-    do  
-        echo "  <index value=\""$index"\">" >> $3 
-        find -L $2/$index/ -type f 2>/dev/null | sed 's/.*\/\(.*\)\.xml/\1/' | sort -n | sed 's/\(.*\)/    <id>\1<\/id>/' >> $3        
+    do
+        echo "  <index value=\""$index"\">" >> $3
+        find -L $2/$index/ -type f 2>/dev/null | sed 's/.*\/\(.*\)\.xml/\1/' | sort -n | sed 's/\(.*\)/    <id>\1<\/id>/' >> $3
         echo "  </index>" >> $3
     done
 
@@ -74,5 +74,9 @@ then
     exit
 fi
 
-echo "Indexing WikiXML" $1 "pages"    
+echo "Indexing WikiXML" $1 "pages"
+
+dir=$(dirname $output)
+mkdir -p $dir
+
 createList $pageType $xmlFolder $output
